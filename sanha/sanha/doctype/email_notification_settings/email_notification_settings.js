8e91aa69aa808018eeb823151a93f05ce9a71323
galaxlabs <galaxylab2020@gmail.com>
2026-01-11 12:29:03 +0000

    patch:

---
+++ sanha/sanha/doctype/email_notification_settings/email_notification_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Sanha Halal Pakistan  and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Email Notification Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
